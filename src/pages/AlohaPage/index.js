764cdfe5c9ea0af8ca62c4f417207714a539d161
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-11 10:02:03 +0200

    sorted users

--- src/pages/AlohaPage/index.js
+++ src/pages/AlohaPage/index.js
@@ -10,10 +10,22 @@ class AlohaPage extends Component {
     }
 
     mapUsers = () => this.state.users.map(user => <Aloha name={user} />)
+
+    sortingUsers = () => {
+        // відсортувати масив, який містить дані (на яких заснований  UI)
+       // оскільки метод sort сортує на місці, а мутувати стан нам не можна, треба зробити копію масива
+       const copy = [...this.state.users];
+       copy.sort( (a,b) => a.toLowerCase > b.toLowerCase ? 1 : -1)
+        // оновити state, просигналізувавши, що у мене змінився стан
+        this.setState({
+            users: copy
+        })
+    }
     
     render() {
         return (
             <section>
+                <button onClick={this.sortingUsers}>Sort users</button>
                 {this.mapUsers()}
             </section>
         );
