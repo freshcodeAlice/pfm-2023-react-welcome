852a0834d4a4e235f521cbb794adb0056ecca10b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-11 12:02:04 +0200

    animations add!

--- src/components/Aloha/index.js
+++ src/components/Aloha/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import 'animate.css';
 
 class Aloha extends React.Component { 
     constructor(props) {
@@ -19,7 +20,7 @@ class Aloha extends React.Component {
     render() {
         const hreetingText = this.state.isGreeting ? 'Hello' : 'Bye';
         return (<h2 
-            className="red-big-text"
+            className="red-big-text animate__animated animate__bounceIn"
             onClick={this.clickHandler}
             >
                 {hreetingText}, {this.props.name}
