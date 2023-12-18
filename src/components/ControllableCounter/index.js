8ad62de59bc99853530e0e2ae685f74742258507
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-18 09:56:09 +0200

    PureComponent

--- src/components/ControllableCounter/index.js
+++ src/components/ControllableCounter/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Component } from 'react';
+import PropTypes from 'prop-types';
 
-class ControllableCounter extends Component {
+class ControllableCounter extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -8,13 +9,56 @@ class ControllableCounter extends Component {
         }
     }
 
+    // shouldComponentUpdate(nextProps, nextState){
+    //     // nextProps, nextState - нові дані, які приходять і на їхній основі ми визначаємо, чи потребує компонента оновлення
+    //     // this.props, this.state - поточні пропси і стейти, які актуальні до оновлення
+    //     // return true - компонента оновлюється
+    //     // return false - компонента не оновлюється
+    // }
+
+    /*
+    PureComponent - різновид React.Component, який базується на ідеї детермінованості (за одних і тих самих вхідних даних отримуємо один і той самий результат). За одних і тих самих пропсах або стейтах ми не перерендерюємо компоненту.
+
+    PureComponent сам реалізує shouldComponentUpdate (тобто ви самі його реалізувати тут не можете)
+    він поверхнево порівнює пропси та стейти і запускає оновлення тільки тоді, коли вони реально помінялись
+
+    PureComponent порівнює стейти і пропси ПОВЕРХНЕВО
+    у випадку:
+
+    const prevState = {
+        engine: {
+            volume: {
+                scale: 'l',
+                value: 20
+            }
+        }  
+    }
+
+        const nextState = {
+        engine: {
+            volume: {
+                scale: 'l',
+                value: 20
+            }
+        }
+    }
+
+    При глибокій вкладеності стейту або пропсів PureComponent не зможе оптимізувати пере-рендер, бо порівнює за посиланнями вкладені об'єкти
+
+
+    Використання PureComponent має сенс, коли виконується дві умови:
+    1. Пропси і стейти - не вкладені об'єкти (не мають глибокої вкладеності)
+    2. У рендері компоненти беруть участь ВСІ стейти і пропси
+    */
+
+
     increment = () => {
         // this.setState({
         //     count: this.state.count + 1
         // })
         this.setState((state, props) => {
             const newState = {
-                count: state.count + 1
+                count: state.count + props.step
             };
 
             return newState
@@ -22,20 +66,43 @@ class ControllableCounter extends Component {
     }
 
     decrement = () => {
+        this.setState((state, props) => {
+            const newState = {
+                count: state.count - props.step
+            };
+
+            return newState
+        })
+    }
+
+    change = () => {
         this.setState({
-            count: this.state.count - 1
+            count: this.state.count
         })
     }
     
     render() {
+        console.log('child re-render')
         return (
             <div>
-                <h1>{this.state.count}</h1>
+                <h1>count: {this.state.count}</h1>
+                <h2>step: {this.props.step}</h2>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
+
+                <button onClick={this.change}>Re-render</button>
             </div>
         );
     }
+}
+
+
+ControllableCounter.propTypes = {
+    step: PropTypes.number
+}
+
+ControllableCounter.defaultProps = {
+    step: 5
 }
 
 export default ControllableCounter;
--- src/pages/CounterPage/index.js
+++ src/pages/CounterPage/index.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import ControllableCounter from '../../components/ControllableCounter';
 
 class CounterPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 0
+        }
+    }
+    
+    changeHandler = ({target: {name, value}}) => {
+        this.setState({
+            [name]: Number(value)
+        })
+    }
+
     render() {
+        console.log('parent re-render')
         return (
             <div>
-                <ControllableCounter />
+                <input 
+                type="range" 
+                value={this.state.step}
+                name='step'
+                onChange={this.changeHandler}/>
+                <span>{this.state.step}</span>
+                <ControllableCounter step={this.state.step}/>
             </div>
         );
     }
