d52694c94bbd82cb48ec0743be982146da843f08
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-18 10:04:38 +0200

    shouldComponentUpdate

--- src/components/ControllableCounter/index.js
+++ src/components/ControllableCounter/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent, Component } from 'react';
 import PropTypes from 'prop-types';
 
-class ControllableCounter extends PureComponent {
+class ControllableCounter extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -9,12 +9,17 @@ class ControllableCounter extends PureComponent {
         }
     }
 
-    // shouldComponentUpdate(nextProps, nextState){
-    //     // nextProps, nextState - нові дані, які приходять і на їхній основі ми визначаємо, чи потребує компонента оновлення
-    //     // this.props, this.state - поточні пропси і стейти, які актуальні до оновлення
-    //     // return true - компонента оновлюється
-    //     // return false - компонента не оновлюється
-    // }
+    shouldComponentUpdate(nextProps, nextState){
+        // nextProps, nextState - нові дані, які приходять і на їхній основі ми визначаємо, чи потребує компонента оновлення
+        // this.props, this.state - поточні пропси і стейти, які актуальні до оновлення
+        // return true - компонента оновлюється
+        // return false - компонента не оновлюється
+
+        if ((nextProps.step !== this.props.step) || (nextState.count === this.state.count)) {
+            return false
+        }
+        return true
+    }
 
     /*
     PureComponent - різновид React.Component, який базується на ідеї детермінованості (за одних і тих самих вхідних даних отримуємо один і той самий результат). За одних і тих самих пропсах або стейтах ми не перерендерюємо компоненту.
@@ -86,7 +91,6 @@ class ControllableCounter extends PureComponent {
         return (
             <div>
                 <h1>count: {this.state.count}</h1>
-                <h2>step: {this.props.step}</h2>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
 
