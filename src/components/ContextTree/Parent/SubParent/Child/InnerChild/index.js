85159a2bed21f839c6ce00a43043bb452fbfbb04
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-19 11:44:27 +0200

    double render-hell

--- src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
+++ src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import UserContext from '../../../../../../contexts/UserContext';
+import ThemeContext from '../../../../../../contexts/ThemeContext';
 
 const InnerChild = (props) => {
     console.log('InnerChild re-render')
     return (
-        <UserContext.Consumer>
-            {([user, logOut]) => {
-                console.log('внутрішня функція-дитина, яка керує рендером компоненти')
+        <ThemeContext.Consumer>
+            {([theme, changeTheme]) => {
                 return (
-                    <div style={{ border: 'inherit', padding: '25px' }}>
-                        InnerChild
-                        <p>
-                         {user?.firstName} {user?.lastName}
-                        </p>
-                        <button onClick={logOut}>LogOut</button>
-                    </div>
-                )
-            }}
-        </UserContext.Consumer>
+                <UserContext.Consumer>
+                {([user, logOut]) => {
+                    console.log('внутрішня функція-дитина, яка керує рендером компоненти')
+                    return (
+                        <div style={{ border: 'inherit', padding: '25px' }}>
+                            InnerChild
+                            <p>
+                             {user?.firstName} {user?.lastName}
+                            </p>
+                            <button onClick={logOut}>LogOut</button>
+                            <button onClick={changeTheme}>ChangeTheme</button>
+                        </div>
+                    )}}
+                </UserContext.Consumer>
+                )}}
+        </ThemeContext.Consumer>
     );
 }
 
