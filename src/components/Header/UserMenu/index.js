b287c3e7d051aebfbe967e33b717e3fb91176aad
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-19 11:58:00 +0200

    ThemeContext task resolve

--- src/components/Header/UserMenu/index.js
+++ src/components/Header/UserMenu/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import UserContext from '../../../contexts/UserContext';
+import ThemeContext from '../../../contexts/ThemeContext';
 
 const UserMenu = () => {
     return (
-       
-           <UserContext.Consumer>
-            {([user, logOut]) => {
-                console.log(user);
-                return (
-                    <div style={{width: '200px', border: '2px solid blue'}}>
-                    <p>{user.firstName} {user.lastName}</p>
-                    </div>)
-            }}
-            </UserContext.Consumer> 
-       
+       <ThemeContext.Consumer>
+        {([theme, setTheme]) => {
+            return (
+                <UserContext.Consumer>
+                {([user, logOut]) => {
+                    console.log(user);
+                    return (
+                        <div style={{width: '200px', border: '2px solid blue'}}>
+                        <p>{user.firstName} {user.lastName}</p>
+                        <p>Theme: {theme}</p>
+                        </div>)
+                }}
+                </UserContext.Consumer> 
+            )
+        }}
+       </ThemeContext.Consumer>       
     );
 }
 
