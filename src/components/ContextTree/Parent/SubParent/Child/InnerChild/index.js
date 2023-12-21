9dc2a5f42ca1831acd5c5d4dac99eff46cf79f97
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-21 10:09:51 +0200

    HOC task resolve

--- src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
+++ src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import UserContext from '../../../../../../contexts/UserContext';
-import ThemeContext from '../../../../../../contexts/ThemeContext';
+import withUser from '../../../../../../HOC/withUser';
+import withTheme from '../../../../../../HOC/withTheme';
 
 const InnerChild = (props) => {
     console.log('InnerChild re-render')
     return (
-        <ThemeContext.Consumer>
-            {([theme, changeTheme]) => {
-                return (
-                <UserContext.Consumer>
-                {([user, logOut]) => {
-                    console.log('внутрішня функція-дитина, яка керує рендером компоненти')
-                    return (
-                        <div style={{ border: 'inherit', padding: '25px' }}>
-                            InnerChild
-                            <p>
-                             {user?.firstName} {user?.lastName}
-                            </p>
-                            <button onClick={logOut}>LogOut</button>
-                            <button onClick={changeTheme}>ChangeTheme</button>
-                        </div>
-                    )}}
-                </UserContext.Consumer>
-                )}}
-        </ThemeContext.Consumer>
-    );
+        <div style={{ border: 'inherit', padding: '25px' }}>
+            InnerChild
+                <p>
+                    {props.user?.firstName} {props.user?.lastName}
+                </p>
+            <button onClick={props.logOut}>LogOut</button>
+            <button onClick={props.changeTheme}>ChangeTheme</button>
+            </div>
+    )
 }
 
-export default InnerChild;
+export default withTheme(withUser(InnerChild));
