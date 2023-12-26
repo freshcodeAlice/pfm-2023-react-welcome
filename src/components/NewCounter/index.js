c59fd34f10db1eaa28ef0be0a6127080062e25d7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-26 09:40:35 +0200

    naming

--- src/components/NewCounter/index.js
+++ src/components/NewCounter/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 const NewCounter = () => { // функціональна компонента - хук використовувати можна
     // ось це - найвищий рівень
-    const [state, setState] = useState(0);
+    const [count, setCount] = useState(0);
 // На відміну від класового стану, тут state - не об'єкт, а той тип даних, який ви туди поклали
 
     /*
@@ -19,17 +19,17 @@ const NewCounter = () => { // функціональна компонента - 
 
 
     const increment = () => {
-        setState(state+1)
+        setCount(count+1)
     }
 
     const decrement = () => {
-        setState(state-1)
+        setCount(count-1)
     }
 
 
     return (
         <div>
-           <h2>{state}</h2> 
+           <h2>{count}</h2> 
            <button onClick={increment}>+</button>
            <button onClick={decrement}>-</button>
         </div>
