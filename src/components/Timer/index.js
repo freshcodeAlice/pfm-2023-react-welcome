0cda9cd45f6ff0f7c299ad467d26f8daaa8685c9
freshcodeAlice <freshjs.ofte@gmail.com>
2023-12-28 09:34:40 +0200

    timer to hooks task resolve

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,72 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {format, addSeconds} from 'date-fns';
 
-class Timer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            time: new Date(0,0,0,0,0,0,0,0),
-            isRunning: false
-        }
-        this.intervalId = null;
-    }
+function Timer (props) {
+    const [time, setTime] = useState( new Date(0,0,0,0,0,0,0,0));
+    const [isRunning, setRunning] = useState( false);           
+    let intervalId = null;
 
-    componentDidMount() {
- //       this.start();
-    }
+
+
+    useEffect(() => {
+        start()
+        return stop;
+    }, [])
     
-    start = () => {
-        if (!this.state.isRunning) {
-            this.intervalId = setInterval(() => {
-
-                // const newDate = new Date(this.state.time.getTime());
-                // newDate.setSeconds(this.state.time.getSeconds() + 1)
-
-                this.setState({
-                    time: addSeconds(this.state.time, 1),
-                    isRunning: true
-                })
+   const start = () => {
+        if (!isRunning) {
+            intervalId = setInterval(() => {
+                setTime(addSeconds(time, 1));
+                setRunning(true);
             }, 1000);
         }
 
     }
-    // відрефакторити і не допустити навішування декількох інтеврвалів
 
-
-    stop = () => {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-        this.setState({
-            isRunning: false
-        })
+    const stop = () => {
+        clearInterval(intervalId);
+        intervalId = null;
+        setRunning(false);
     }
 
-    componentWillUnmount() {
-        this.stop();
-    }
 
-    restart = () => {
-
-        this.setState({
-            time: new Date(0,0,0,0,0,0,0,0)
-        })
+    const restart = () => {
+        setTime(new Date(0,0,0,0,0,0,0,0))
     }
 
     /// Зробити одну кнопку, на якій відображається "Start", якщо таймер зупинений або "Stop" якщо він іде
  
-    render() {
-        const callback = this.state.isRunning ? this.stop : this.start;
-        const textButton = this.state.isRunning ? 'Stop' : 'Start';
+
+        const callback = isRunning ? stop : start;
+        const textButton = isRunning ? 'Stop' : 'Start';
         return (
             <article>
-                <h1>{format(this.state.time, 'HH:mm:ss')}</h1>
+                <h1>{format(time, 'HH:mm:ss')}</h1>
                 {/* <button onClick={this.start}>Start</button>
                 <button onClick={this.stop}>Stop</button> */}
                 <button onClick={callback}>{textButton}</button>
-                <button onClick={this.restart}>Restart</button>
+                <button onClick={restart}>Restart</button>
             </article>
         );
-    }
+
 }
 
 export default Timer;
